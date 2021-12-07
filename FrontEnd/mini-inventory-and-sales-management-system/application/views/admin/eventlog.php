0fed445ff9a1341b472b4f4c9f764d0403aac23c
Santiago Alejandro Vargas Céspedes <60782047+AleS900@users.noreply.github.com>
2021-12-07 07:38:45 -0400

    Add files via upload

---
+++ FrontEnd/mini-inventory-and-sales-management-system/application/views/admin/eventlog.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('');
+?>
+
+<div class="row">
+    <div class="col-sm-12">
+        <div class="well">
+            Registro de eventos
+        </div>
+    </div>
+</div>
